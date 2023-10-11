cbe6047ce8b7b6374b7bb9fa740e3c4b147a91ac
cmpleitez <cmpleitez.sv@gmail.com>
2023-10-11 12:47:42 -0600

    sync

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
 
-                    <a href="{{route('project.create')}}">create</a>
+                    <a href="{{route('project.index')}}">create</a>
 
                 </div>
             </div>
