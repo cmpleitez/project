3c273a3906400110332b26ca33bc4344c4c761dd
cmpleitez <cmpleitez.sv@gmail.com>
2023-11-10 14:11:37 -0600

    sync

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProyectoController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\roleController;
